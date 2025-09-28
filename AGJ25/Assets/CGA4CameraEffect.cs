a221134bcd711265504118264ff14d220b65cd10
Dave Goel <davegoel29@gmail.com>
2025-09-28 11:42:06 -0600

    added shader

---
+++ AGJ25/Assets/CGA4CameraEffect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CGA4CameraEffect : MonoBehaviour {
+
+	public Material material;
+
+	// Creates a private material used to the effect
+	void Awake () {
+		//material = new Material( Shader.Find("Hidden/CGAHalftone") );
+	}
+
+	// Postprocess the image
+	void OnRenderImage (RenderTexture source, RenderTexture destination) {
+		Graphics.Blit (source, destination, material);
+	}
+}
